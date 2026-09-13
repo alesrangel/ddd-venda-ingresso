f2f802a743d61618dd53c166c13aada469728d1c
alesrangel <alesrangel15@gmail.com>
2026-09-12 22:58:40 -0300

    value object

---
+++ src/@core/common/domain/value-objects/name.vo.spec.ts
@@ -0,0 +1,15 @@
+import { Customer } from "../../../events/domain/entities/customer.entity";
+import { Name } from "./name.vo";
+
+
+test('deve criar nome valido', () => {
+     const name = new Name('aaaaa');
+     expect(name.value).toBe('aaaaa');
+
+     const customer = new Customer({
+          cpf: '111.111.111-11',
+          name,
+     });
+
+     customer.name = new Name('bbbbb');
+})
--- src/@core/events/domain/entities/customer.entity.ts
+++ src/@core/events/domain/entities/customer.entity.ts
@@ -1,25 +1,26 @@
-import { randomUUID } from "crypto";
-import { AggregateRoot } from "../../../common/domain/aggregate-root.js";
+import { randomUUID } from "node:crypto";
+import { AggregateRoot } from "../../../common/domain/aggregate-root";
+import { Name } from "../../../common/domain/value-objects/name.vo";
 
 export type CustomerConstructorProps = {
-    id: string;
+    id?: string;
     cpf: string;
-    name: string;
+    name: Name;
 };
 
 export class Customer extends AggregateRoot{
     id: string;
     cpf: string;
-    name: string;
+    name: Name;
 
     constructor(props: CustomerConstructorProps){
         super();
-        this.id = props.id;
+        this.id = props.id ?? randomUUID();
         this.cpf = props.cpf;
         this.name = props.name;
     }
 
-    static create(command: {name: string, cpf: string}){
+    static create(command: {name: Name, cpf: string}){
         return new Customer({id: randomUUID() ,...command});
     }
 
